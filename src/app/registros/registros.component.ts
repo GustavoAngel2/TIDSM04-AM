0d0bcae8c41a4b3968fda20663aa69ad35acebd1
GustavoAngel2 <12755@utl.edu.mx>
2023-10-20 07:46:09 -0600

    Tabla

--- src/app/registros/registros.component.ts
+++ src/app/registros/registros.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 
-export interface PeriodicElement {
+export interface Personas {
   nombre: string;
   matricula: number;
   Curp: string;
-  numcel: string;
+  numcel: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {matricula: 1, nombre: 'Hydrogen', Curp: '1.0079', numcel: 'H'},
-  {matricula: 2, nombre: 'Helium', Curp: '4.0026', numcel: 'He'},
-  {matricula: 3, nombre: 'Lithium', Curp: '6.941', numcel: 'Li'},
-  {matricula: 4, nombre: 'Beryllium', Curp: '9.0122', numcel: 'Be'},
-  {matricula: 5, nombre: 'Boron', Curp: '10.811', numcel: 'B'},
-  {matricula: 6, nombre: 'Carbon', Curp: '12.0107', numcel: 'C'},
-  {matricula: 7, nombre: 'Nitrogen', Curp: '14.0067', numcel: 'N'},
-  {matricula: 8, nombre: 'Oxygen', Curp: '15.9994', numcel: 'O'},
-  {matricula: 9, nombre: 'Fluorine', Curp: '18.9984', numcel: 'F'},
-  {matricula: 10, nombre: 'Neon', Curp: '20.1797', numcel: 'Ne'},
+const ELEMENT_DATA: Personas[] = [
+  {matricula: 12755, nombre: 'Gustavo', Curp: 'BORG040523HNLRMSA7', numcel: 8211202886},
+  {matricula: 12781, nombre: 'Brandon', Curp: 'SAGB990213HNLRNR06', numcel: 8211110434},
+  {matricula: 12762, nombre: 'Jonathan', Curp: 'COPJ040126HVZRRNA4', numcel: 8287686734},
+  {matricula: 12780, nombre: 'Yeretzi', Curp: 'SAVY040926MNLNZRA0', numcel: 5628414013},
+  {matricula: 12782, nombre: 'José Reynaldo', Curp: 'SIGR040605HNLLRYA8', numcel: 8135585770},
+  {matricula: 12772, nombre: 'Alejandra', Curp: 'LUZA010301MNLCXLA2', numcel: 8212116688},
+  {matricula: 12753, nombre: 'Carlos Alberto', Curp: 'AAMC041101HNLLNRA0', numcel: 8443107971},
+  {matricula: 12752, nombre: 'Karen', Curp: 'AAMK030619MNLLRRB6', numcel: 8211066839},
+  {matricula: 12761, nombre: 'Carlos', Curp: 'COLC010315HCHRPRA2', numcel: 6761141373},
+  {matricula: 12763, nombre: 'Bryan', Curp: 'ROMB041124HNLSDRA9', numcel: 8211028403},
+  {matricula: 12765, nombre: 'Jose Manuel', Curp: 'FUIM050817HNLNRNA0', numcel: 6761141373}
 ];
 
 @Component({
@@ -30,6 +31,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 
 export class RegistrosComponent {
-  displayedColumns: string[] = ['Matricula', 'Nombre', 'CURP', 'Numero de Celular'];
+  displayedColumns: string[] = ['matricula', 'nombre', 'Curp', 'numcel'];
   dataSource = ELEMENT_DATA;
 }
